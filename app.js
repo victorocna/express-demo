597f24f22e3ee5c03cc63e894fd3ccf7cd12bcba
c0bqlt <103998296+c0bqlt@users.noreply.github.com>
2022-08-01 19:55:57 +0300

    upload

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+
 
 app.get("/", (req, res) => {
   // TODO: read all players
+  fs.readFile("./players.json", "utf-8", (err, jsonString) => {
+    if (err) {
+      return console.log("File read failed: ", err);
+    }
+    console.log("File data: ", jsonString);
+  });
+  // let data = "";
+  // res.on("data", (chunk) => {
+  //   data += chunk;
+  // });
+
+  // res.on("end", () => {
+  //   console.log(JSON.parse(data).explanation);
+  // });
+  // res.send("Hello world!");
 });
+//   .on("error", (err) => {
+//     console.log("Error " + err.message);
+//   });
 
 app.post("/", (req, res) => {
-  // TODO: write player to file
+  //   // TODO: write player to file
 });
 
 app.get("/:id", (req, res) => {
-  // TODO: read player by id
+  //   // TODO: read player by id
 });
 
 module.exports = app;
